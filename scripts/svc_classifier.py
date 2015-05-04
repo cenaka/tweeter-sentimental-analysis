6b83f60cbe07ebf6a0ee737f260e8ada483a641b
cenaka <cenaka@yandex.ru>
2015-05-04 15:17:11 +0300

    time counting added

--- scripts/svc_classifier.py
+++ scripts/svc_classifier.py
@@ -7,22 +7,27 @@ __author__ = 'cenaka'
 import numpy as np
 from sklearn import svm
 from sklearn.metrics import classification_report
-
+import datetime
 def fit_svc(feature_matrix, answers):
-    svc = svm.SVC(gamma=0.001, C=100.)
+    svc = svm.SVC(gamma=0.001, C=100., verbose=True)
     return svc.fit(feature_matrix, answers)
 
 
 def read_feature_matrix_from_file(file_path):
-    return np.loadtxt(file_path, delimiter=";")
+    return np.loadtxt(file_path)
 
 
-def generate_answers():
+def generate_traning_answers():
     """
     Обучающая и тестовая выборки из примера содержат по 100 твитов: первые 50 негативных, вторые 50 позитивных
     """
-    return np.concatenate((np.array([NEGATIVE_CLASS] * 50), np.array([POSITIVE_CLASS] * 50)))
+    return np.concatenate((np.array([POSITIVE_CLASS] * 100000), np.array([NEGATIVE_CLASS] * 100000)))
 
+def generate_test_answers():
+    """
+    Обучающая и тестовая выборки из примера содержат по 100 твитов: первые 50 негативных, вторые 50 позитивных
+    """
+    return np.concatenate((np.array([POSITIVE_CLASS] * 100000), np.array([NEGATIVE_CLASS] * 100000)))
 
 def evaluate(classifier, test_features, answers):
     predicted = [classifier.predict(test)[0] for test in test_features]
@@ -31,7 +36,9 @@ def evaluate(classifier, test_features, answers):
 
 
 if __name__ == "__main__":
-    classifier = fit_svc(read_feature_matrix_from_file(TRAIN_FEATURES_FILE_NAME), generate_answers())
+    time = datetime.datetime.now()
+    classifier = fit_svc(read_feature_matrix_from_file(TRAIN_FEATURES_FILE_NAME), generate_traning_answers())
+    print("fit model: seconds_passed: %s" % (datetime.datetime.now() - time).total_seconds())
     test_features = read_feature_matrix_from_file(TEST_FEATURES_FILE_NAME)
-    answers = generate_answers()
+    answers = generate_test_answers()
     evaluate(classifier, test_features, answers)
