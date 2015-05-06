a52e02a3a04c70349b9a4413585e66990d54dd3a
cenaka <cenaka@yandex.ru>
2015-05-06 14:02:14 +0300

    sparse matrices added

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -1,31 +1,31 @@
 import numpy as np
 import re
 from sklearn.feature_extraction.text import CountVectorizer
+import scipy.sparse as sp
+from scipy import sparse, io
 import datetime
-import csv
 
 # FEATURE_EXTRACTORS
+import sys
 from samples import TRAIN_TWEETS_FILE_NAME, TRAIN_FEATURES_FILE_NAME, TEST_TWEETS_FILE_NAME, TEST_FEATURES_FILE_NAME, \
     POS_TWEETS_FILE_NAME, NEG_TWEETS_FILE_NAME, TEST_UNIGRAMMS_FEATURES_FILE_NAME, TRAIN_UNIGRAMMS_FEATURES_FILE_NAME, \
     TEST_OTHER_FEATURES_FILE_NAME, TRAIN_OTHER_FEATURES_FILE_NAME, STOP_WORDS
 
+from tweet_prepare import clean_tweet
+
+NO_WORD_REGEXP = re.compile(r"\bне\b")
+URL_REGEXP = re.compile(r"http(s)?://")
 
 def has_exclamation_mark(tweet):
-    return '!' in tweet
-
+    return int('!' in tweet)
 
 def has_word_not(tweet):
-    pattern = "[^а-яА-Я0-9_]?не[^а-яА-Я0-9_]"
-    regexp = re.compile(pattern)
-    return len(regexp.findall(tweet))
-
+    return int(bool(NO_WORD_REGEXP.search(tweet)))
 
 def has_url(tweet):
-    pattern = "http://"
-    regexp = re.compile(pattern)
-    return len(regexp.findall(tweet)) > 0
+    return int(bool(URL_REGEXP.search(tweet)))
 
-FEATURE_EXTRACTORS = [has_word_not, has_exclamation_mark, len]
+FEATURE_EXTRACTORS = [has_word_not, has_exclamation_mark, has_url, len]
 # FEATURE EXTRACTION
 
 
@@ -50,16 +50,29 @@ def delete_nickname(tweet):
         return tweet
 
 
+def clean_tweets(tweets):
+    cleaned_tweets = []
+    for i, tweet in enumerate(tweets.readlines()):
+        cleaned_tweets.append(clean_tweet(tweet))
+        if i % 1000 == 0:
+            print("cleaned {} tweets".format(i))
+    return cleaned_tweets
+
+
 def extract_uni_features_from_tweets(tweets, test_tweets, output_training_filepath, output_test_filepath):
     stop_words = [line.rstrip() for line in open(STOP_WORDS)]
     print(stop_words)
-    vectorSK = CountVectorizer(min_df=1, max_features=500, stop_words=stop_words)
-    features_matrix = vectorSK.fit_transform([delete_nickname(tweet) for tweet in tweets.readlines()])
-    test_matrix = vectorSK.transform(test_tweets.readlines())
+    vectorSK = CountVectorizer(min_df=1, max_features=5000, stop_words=stop_words)
+    features_matrix = vectorSK.fit_transform(clean_tweets(tweets))
+    test_matrix = vectorSK.transform(clean_tweets(test_tweets))
     for x in vectorSK.get_feature_names():
         print(x)
-    np.savetxt(output_training_filepath, np.array(features_matrix.toarray()), fmt="%d")
-    np.savetxt(output_test_filepath, np.array(test_matrix.toarray()), fmt="%d")
+    io.mmwrite(output_training_filepath, features_matrix)
+    print("unigrams training matrix dumped")
+    io.mmwrite(output_test_filepath, test_matrix)
+    print("unigrams test matrix dumped")
+    #np.savetxt(output_training_filepath, np.array(features_matrix.toarray()), fmt="%d")
+    #np.savetxt(output_test_filepath, np.array(test_matrix.toarray()), fmt="%d")
 
 
 def extract_and_dump_features(input_filepath, output_filepath):
@@ -94,6 +107,14 @@ def make_test_and_training_set(pos_tweets, neg_tweets, output_training_file, out
             else:
                 break
 
+
+def make_all_features_matrix(unigrams_filepath, other_features_filepath, all_features_file_path):
+    sparse_features_matrix = sparse.csr_matrix(np.loadtxt(other_features_filepath))
+    unigrams_matrix = io.mmread(unigrams_filepath)
+    all_features = sp.hstack([sparse_features_matrix, unigrams_matrix])
+    io.mmwrite(all_features_file_path, all_features)
+
+
 if __name__ == '__main__':
     make_test_and_training_set(open(POS_TWEETS_FILE_NAME, encoding="utf8"), open(NEG_TWEETS_FILE_NAME, encoding="utf8"),
                       open(TRAIN_TWEETS_FILE_NAME, 'w'), open(TEST_TWEETS_FILE_NAME, 'w'), 100000)
@@ -102,8 +123,11 @@ if __name__ == '__main__':
     extract_and_dump_features(TEST_TWEETS_FILE_NAME, TEST_OTHER_FEATURES_FILE_NAME)
     extract_and_dump_unigram_features(TRAIN_TWEETS_FILE_NAME, TRAIN_UNIGRAMMS_FEATURES_FILE_NAME, TEST_TWEETS_FILE_NAME,
                                       TEST_UNIGRAMMS_FEATURES_FILE_NAME)
+    make_all_features_matrix(TEST_UNIGRAMMS_FEATURES_FILE_NAME, TEST_OTHER_FEATURES_FILE_NAME, TEST_FEATURES_FILE_NAME)
+    make_all_features_matrix(TRAIN_UNIGRAMMS_FEATURES_FILE_NAME, TRAIN_OTHER_FEATURES_FILE_NAME, TRAIN_FEATURES_FILE_NAME)
+
     print("extract features: seconds_passed: %s" % (datetime.datetime.now() - time).total_seconds())
 
-#paste other_training_feature_matrix.txt unigrams_training_feature_matrix.txt > feature_matrix.txt
-#
+# paste other_training_feature_matrix.txt unigrams_training_feature_matrix.txt > all_training_features.txt
+# paste other_test_feature_matrix.txt unigrams_test_feature_matrix.txt > all_test_features.txt
 
--- scripts/svc_classifier.py
+++ scripts/svc_classifier.py
@@ -1,4 +1,4 @@
-from samples import TRAIN_FEATURES_FILE_NAME, TEST_FEATURES_FILE_NAME
+from samples import TRAIN_FEATURES_FILE_NAME, TEST_FEATURES_FILE_NAME, CLASSIFIER, TRAIN_OTHER_FEATURES_FILE_NAME, TEST_OTHER_FEATURES_FILE_NAME
 
 POSITIVE_CLASS = "pos"
 NEGATIVE_CLASS = "neg"
@@ -8,37 +8,49 @@ import numpy as np
 from sklearn import svm
 from sklearn.metrics import classification_report
 import datetime
+from sklearn.externals import joblib
+from scipy import io
+N = 100000
+
 def fit_svc(feature_matrix, answers):
     svc = svm.SVC(gamma=0.001, C=100., verbose=True)
     return svc.fit(feature_matrix, answers)
 
 
 def read_feature_matrix_from_file(file_path):
-    return np.loadtxt(file_path)
+    return io.mmread(file_path)
 
 
-def generate_traning_answers():
+def generate_training_answers():
     """
     Обучающая и тестовая выборки из примера содержат по 100 твитов: первые 50 негативных, вторые 50 позитивных
     """
-    return np.concatenate((np.array([POSITIVE_CLASS] * 100000), np.array([NEGATIVE_CLASS] * 100000)))
+    return np.concatenate((np.array([POSITIVE_CLASS] * N), np.array([NEGATIVE_CLASS] * N)))
+
 
 def generate_test_answers():
     """
     Обучающая и тестовая выборки из примера содержат по 100 твитов: первые 50 негативных, вторые 50 позитивных
     """
-    return np.concatenate((np.array([POSITIVE_CLASS] * 100000), np.array([NEGATIVE_CLASS] * 100000)))
+    return np.concatenate((np.array([POSITIVE_CLASS] * N), np.array([NEGATIVE_CLASS] * N)))
+
 
 def evaluate(classifier, test_features, answers):
-    predicted = [classifier.predict(test)[0] for test in test_features]
+    predicted = classifier.predict(test_features)
     target_names = [POSITIVE_CLASS, NEGATIVE_CLASS]
     print(classification_report(answers, predicted, target_names=target_names))
 
 
 if __name__ == "__main__":
     time = datetime.datetime.now()
-    classifier = fit_svc(read_feature_matrix_from_file(TRAIN_FEATURES_FILE_NAME), generate_traning_answers())
+    classifier = fit_svc(read_feature_matrix_from_file(TRAIN_FEATURES_FILE_NAME), generate_training_answers())
+
+    joblib.dump(classifier, CLASSIFIER)
+    #classifier = joblib.load(CLASSIFIER)
     print("fit model: seconds_passed: %s" % (datetime.datetime.now() - time).total_seconds())
+    time = datetime.datetime.now()
     test_features = read_feature_matrix_from_file(TEST_FEATURES_FILE_NAME)
+
     answers = generate_test_answers()
     evaluate(classifier, test_features, answers)
+    print("evaluate answers: seconds_passed: %s" % (datetime.datetime.now() - time).total_seconds())
