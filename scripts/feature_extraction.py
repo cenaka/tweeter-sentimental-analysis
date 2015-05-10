054bf605b47fc4f12a8f5a2632ccb5fe74811be5
cenaka <cenaka@yandex.ru>
2015-05-10 13:39:05 +0300

    splitting test/train sets changed

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -13,46 +13,60 @@ from samples import TRAIN_TWEETS_FILE_NAME, TRAIN_FEATURES_FILE_NAME, TEST_TWEET
 
 from tweet_prepare import clean_tweet
 
+# FEATURE_EXTRACTORS
 NO_WORD_REGEXP = re.compile(r"\bне\b")
 URL_REGEXP = re.compile(r"http(s)?://")
-BIG_WORDS_REGEXP = re.compile(r"\b[^a-zA-Zа-я\W\s\d]+\b")
+BIG_WORDS_REGEXP = re.compile(r"\b[^a-zа-я\W\s\d]+\b")
 REPEAT_LETTERS_REGEXP = re.compile(r"(\w)\1\1")
 REPEAT_TWO_LETTERS_REGEXP = re.compile(r"([^\s\d]{2})\1\1")
 REPEAT_THREE_LETTERS_REGEXP = re.compile(r"([^\s\d]{3})\1")
 REPEAT_FOUR_LETTERS_REGEXP = re.compile(r"([^\s\d]{4})\1")
-REPEAT_EXCLAMATION_MARK = re.compile(r"!(!+|1{2})")
+REPEAT_EXCLAMATION_MARK = re.compile(r"[A-Za-zА-Яа-я0-9][!|1](!+|1{2})")
+
+
 
 def has_exclamation_mark(tweet):
     return int('!' in tweet)
 
+
 def has_repeat_exclamation_mark(tweet):
     return int(bool(REPEAT_EXCLAMATION_MARK.search(tweet)))
+
 
 def has_question_mark(tweet):
     return int('?' in tweet)
 
+
 def has_word_not(tweet):
     return int(bool(NO_WORD_REGEXP.search(tweet)))
+
 
 def has_url(tweet):
     return int(bool(URL_REGEXP.search(tweet)))
 
+
 def has_big_word(tweet):
     return int(bool(BIG_WORDS_REGEXP.search(tweet)))
+
 
 def has_repeat_letters(tweet):
     return int(bool(REPEAT_LETTERS_REGEXP.search(tweet)))
 
+
 def has_two_repeat_letters(tweet):
     return int(bool(REPEAT_TWO_LETTERS_REGEXP.search(tweet)))
+
 
 def has_tree_repeat_letters(tweet):
     return int(bool(REPEAT_THREE_LETTERS_REGEXP.search(tweet)))
 
+
 def has_four_repeat_letters(tweet):
     return int(bool(REPEAT_FOUR_LETTERS_REGEXP.search(tweet)))
 
-FEATURE_EXTRACTORS = [has_word_not, has_exclamation_mark, has_repeat_exclamation_mark, has_question_mark, has_url, has_big_word, has_repeat_letters, has_two_repeat_letters, has_tree_repeat_letters, has_four_repeat_letters, len]
+FEATURE_EXTRACTORS = [has_word_not, has_exclamation_mark, has_repeat_exclamation_mark, has_question_mark, has_url,
+                      has_big_word, len]
+
 # FEATURE EXTRACTION
 
 
@@ -67,7 +81,7 @@ def extract_features_from_tweets(tweets, output_file):
         output_file.write(" ".join(map(str, extract_features_from_tweet(tweet))))
         output_file.write("\n")
         if tweet_number % 10000 == 0:
-            print(tweet_number, tweet)
+            print(tweet_number)
 
 
 def delete_nickname(tweet):
@@ -89,11 +103,12 @@ def clean_tweets(tweets):
 def extract_uni_features_from_tweets(tweets, test_tweets, output_training_filepath, output_test_filepath):
     stop_words = [line.rstrip() for line in open(STOP_WORDS)]
     print(stop_words)
-    vectorSK = CountVectorizer(min_df=1, max_features=5000, stop_words=stop_words)
+    vectorSK = CountVectorizer(min_df=1, stop_words=stop_words)
     features_matrix = vectorSK.fit_transform(clean_tweets(tweets))
     test_matrix = vectorSK.transform(clean_tweets(test_tweets))
-    for x in vectorSK.get_feature_names():
-        print(x)
+    #for x in vectorSK.get_feature_names():
+    #    print(x)
+    print(len(vectorSK.get_feature_names()))
     io.mmwrite(output_training_filepath, features_matrix)
     print("unigrams training matrix dumped")
     io.mmwrite(output_test_filepath, test_matrix)
@@ -112,26 +127,29 @@ def extract_and_dump_unigram_features(input_filepath, output_filepath, input_tes
         output_filepath, output_test_filepath)
 
 
-def make_test_and_training_set(pos_tweets, neg_tweets, output_training_file, output_test_file, number):
+def make_test_and_training_set(pos_tweets, neg_tweets, output_training_file, output_test_file, training_set_size,
+                               test_set_size):
     added_tweets = 0
     for tweet in pos_tweets:
         added_tweets += 1
-        if number >= added_tweets:
+        if training_set_size >= added_tweets:
             output_training_file.write(tweet)
         else:
-            if 2 * number >= added_tweets:
+            if training_set_size + test_set_size >= added_tweets:
                 output_test_file.write(tweet)
             else:
+                print(added_tweets)
                 break
     added_tweets = 0
     for tweet in neg_tweets:
         added_tweets += 1
-        if number >= added_tweets:
+        if training_set_size >= added_tweets:
             output_training_file.write(tweet)
         else:
-            if 2 * number >= added_tweets:
+            if training_set_size + test_set_size >= added_tweets:
                 output_test_file.write(tweet)
             else:
+                print(added_tweets)
                 break
 
 
@@ -143,14 +161,14 @@ def make_all_features_matrix(unigrams_filepath, other_features_filepath, all_fea
 
 
 if __name__ == '__main__':
-    make_test_and_training_set(open(POS_TWEETS_FILE_NAME, encoding="utf8"), open(NEG_TWEETS_FILE_NAME, encoding="utf8"),
-                      open(TRAIN_TWEETS_FILE_NAME, 'w'), open(TEST_TWEETS_FILE_NAME, 'w'), 100000)
+    #make_test_and_training_set(open(POS_TWEETS_FILE_NAME, encoding="utf8"), open(NEG_TWEETS_FILE_NAME, encoding="utf8"),
+    #                  open(TRAIN_TWEETS_FILE_NAME, 'w'), open(TEST_TWEETS_FILE_NAME, 'w'), 4000000, 100000)
     time = datetime.datetime.now()
-    extract_and_dump_features(TRAIN_TWEETS_FILE_NAME, TRAIN_OTHER_FEATURES_FILE_NAME)
-    extract_and_dump_features(TEST_TWEETS_FILE_NAME, TEST_OTHER_FEATURES_FILE_NAME)
-    extract_and_dump_unigram_features(TRAIN_TWEETS_FILE_NAME, TRAIN_UNIGRAMMS_FEATURES_FILE_NAME, TEST_TWEETS_FILE_NAME,
-                                      TEST_UNIGRAMMS_FEATURES_FILE_NAME)
-    make_all_features_matrix(TEST_UNIGRAMMS_FEATURES_FILE_NAME, TEST_OTHER_FEATURES_FILE_NAME, TEST_FEATURES_FILE_NAME)
+    #extract_and_dump_features(TRAIN_TWEETS_FILE_NAME, TRAIN_OTHER_FEATURES_FILE_NAME)
+    #extract_and_dump_features(TEST_TWEETS_FILE_NAME, TEST_OTHER_FEATURES_FILE_NAME)
+    #extract_and_dump_unigram_features(TRAIN_TWEETS_FILE_NAME, TRAIN_UNIGRAMMS_FEATURES_FILE_NAME, TEST_TWEETS_FILE_NAME,
+    #                                  TEST_UNIGRAMMS_FEATURES_FILE_NAME)
+    #make_all_features_matrix(TEST_UNIGRAMMS_FEATURES_FILE_NAME, TEST_OTHER_FEATURES_FILE_NAME, TEST_FEATURES_FILE_NAME)
     make_all_features_matrix(TRAIN_UNIGRAMMS_FEATURES_FILE_NAME, TRAIN_OTHER_FEATURES_FILE_NAME, TRAIN_FEATURES_FILE_NAME)
 
     print("extract features: seconds_passed: %s" % (datetime.datetime.now() - time).total_seconds())
